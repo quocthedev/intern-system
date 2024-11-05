92379f58e3a72c849b9d68200b1d71126ad60277
Kha <khatt12323@gmail.com>
2024-11-05 15:00:56 +0000

    intern period page

---
+++ app/(dashboard)/internPeriod/page.tsx
@@ -0,0 +1,12 @@
+import ActionBar from "@/app/(dashboard)/internPeriod/_components/ActionBar";
+import InternPeroidTable from "@/app/(dashboard)/internPeriod/_components/InternPeroidTable";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <ActionBar />
+      <InternPeroidTable />
+    </div>
+  );
+}
