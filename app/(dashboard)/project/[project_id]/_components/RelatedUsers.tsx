6918bf4974acff2c00b2e1bf12146b3b385b1d3e
Kha <khatt12323@gmail.com>
2024-11-14 17:42:05 +0000

    add RelatedUsers component to display project members with actions

---
+++ app/(dashboard)/project/[project_id]/_components/RelatedUsers.tsx
@@ -0,0 +1,85 @@
+import React, { Key } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/table";
+import NewMemberModal from "./NewMemberModal";
+import MemberUpdate from "./MemberUpdate";
+
+export type RelatedUsersProps = {
+  groups: RelatedUser[];
+  projectId: string;
+  projectName: string;
+};
+
+type RelatedUser = {
+  id: number;
+  name: string;
+  role: string;
+};
+export default function RelatedUsers(props?: RelatedUsersProps) {
+  const renderCell = (item: any, columnKey: Key) => {
+    switch (columnKey) {
+      case "name":
+        return <p className="text-sm">{item.fullName}</p>;
+      case "role":
+        return <p className="text-sm">{item.role}</p>;
+      case "actions":
+        return (
+          <MemberUpdate
+            projectName={props?.projectName as string}
+            memberName={item.fullName}
+            role={item.role}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  const columns = [
+    {
+      key: "name",
+      title: "Name",
+    },
+    {
+      key: "role",
+      title: "Role",
+    },
+    {
+      key: "actions",
+      title: "Actions",
+    },
+  ];
+
+  return (
+    <div className="flex w-full flex-col gap-3">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold">Members</h1>
+        <NewMemberModal projectId={props?.projectId as string} />
+      </div>
+
+      <Table className="max-h-[300px]" isHeaderSticky>
+        <TableHeader columns={columns}>
+          {(column) => (
+            <TableColumn key={column.key}>{column.title}</TableColumn>
+          )}
+        </TableHeader>
+
+        <TableBody items={props?.groups} className="overflow-hidden">
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
