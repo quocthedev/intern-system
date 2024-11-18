cf273fc2f263dc427aa4dd8f99914a80c779ede4
Kha <khatt12323@gmail.com>
2024-11-18 15:46:56 +0000

    ui refine

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ export default function DashBoardLayout(props: DashBoardLayoutProps) {
   return (
     <main className="flex h-screen w-screen bg-dashboard-bg">
       <SideBar />
-      <div className="h-full grow overflow-scroll rounded-3xl bg-white">
+      <div className="h-full w-full overflow-scroll rounded-3xl bg-white">
         {props.children}
       </div>
     </main>
