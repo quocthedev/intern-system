8b79a8b31096c53c2b4dd58c6c6116e26e0d3d31
Kha <khatt12323@gmail.com>
2024-11-19 15:32:55 +0000

    add InterviewSchedule component with modal for adding candidates

---
+++ app/(dashboard)/interview-schedule/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import InterviewScheduleModal from "@/components/InterviewScheduleModal";
+
+export default function InterviewSchedule() {
+  return (
+    <div>
+      <InterviewScheduleModal isAddingCandidate />
+    </div>
+  );
+}
