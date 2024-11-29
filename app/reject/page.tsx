829e8c986166111276a1b9f68371167baa1263be
quocthedev <qklgithub@gmail.com>
2024-11-29 21:44:08 +0700

    fix response email

--- app/reject/page.tsx
+++ app/reject/page.tsx
@@ -1,18 +1,22 @@
 import { Card, CardBody } from "@nextui-org/card";
 import React from "react";
 
+//Hydration bug how to fix is wrap div outside the p tag
+
 export default function page() {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
       <Card>
         <CardBody>
           <form className="flex w-full flex-col gap-3 p-3">
-            <p>
-              Thank you for confirming your attendance for the interview. <br />
-              We’re excited to meet with you and learn more about your skills
-              <br />
-              and experience. We look forward to speaking with you soon! <br />
-            </p>
+            <div>
+              <p>
+                Thank you for your response. <br />
+                We’re sorry to hear you won’t be able to attend the interview.{" "}
+                <br />
+                Wishing you all the best! <br />
+              </p>
+            </div>
           </form>
         </CardBody>
       </Card>
