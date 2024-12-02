47a04ad473a86457cea48fc0af6b4cf2e808e2bb
quocthedev <qklgithub@gmail.com>
2024-12-02 14:34:54 +0700

    change config with server backend

--- libs/config.ts
+++ libs/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://intern-system-amazing-tech.azurewebsites.net/api";
 
 export const API_ENDPOINTS = {
   login: `${API_BASE_URL}/auth/signin`,
