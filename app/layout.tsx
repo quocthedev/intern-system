0c2f0e04257adfed0abf9f8d7e5fe0ca75a0d31c
quocthedev <qklgithub@gmail.com>
2024-12-27 15:37:02 +0700

    change font for systerm

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
-import { poppins } from "@/config/fonts";
+import { poppins, roboto } from "@/config/fonts";
 
 export const metadata: Metadata = {
   title: {
@@ -32,12 +32,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html suppressHydrationWarning lang="en">
+    <html lang="en">
       <head />
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          poppins.className,
+          roboto.className,
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
--- config/fonts.ts
+++ config/fonts.ts
@@ -1,5 +1,5 @@
 import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
-import { Poppins } from "next/font/google";
+import { Poppins, Roboto} from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -13,6 +13,13 @@ export const fontMono = FontMono({
 
 export const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800"], // You can specify different weights
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+
+export const roboto = Roboto({
+  weight: "400",
   subsets: ["latin"],
   variable: "--font-poppins",
 });
