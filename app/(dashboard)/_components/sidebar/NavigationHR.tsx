e09c32552fb6e35615655ba27f37d0d9830dc6c1
quocthedev <qklgithub@gmail.com>
2024-12-30 22:54:47 +0700

    add seperate sidebar for each role

---
+++ app/(dashboard)/_components/sidebar/NavigationHR.tsx
@@ -0,0 +1,62 @@
+"use client";
+import {
+  DashboardIcon,
+  InternManagementIcon,
+  InterviewManagementIcon,
+} from "./Icons";
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
+import { cn } from "@nextui-org/react";
+import { usePathname } from "next/navigation";
+
+type NavigationProps = {
+  className?: string;
+};
+
+export const NavigationItems = [
+  {
+    label: "Dashboard",
+    href: "/",
+    icon: DashboardIcon,
+  },
+  {
+    label: "Intern periods",
+    href: "/internPeriod",
+    icon: InternManagementIcon,
+  },
+  {
+    label: "Interviews",
+    href: "/interview",
+    icon: InterviewManagementIcon,
+  },
+];
+
+export default function NavigationHR(props: NavigationProps) {
+  const currentPath = usePathname();
+
+  return (
+    <div className={cn("flex w-full flex-col gap-4", props.className)}>
+      {NavigationItems.map((item) => {
+        const isActive = item.href === `/${currentPath.split("/")[1]}`;
+
+        return (
+          <Button
+            color="primary"
+            variant={isActive ? "shadow" : "light"}
+            as={Link}
+            startContent={<item.icon size={20} active={isActive} />}
+            className={cn(
+              "w-full justify-start text-sm font-medium text-grey",
+              isActive && "font-semibold text-white",
+            )}
+            href={item.href}
+            size="md"
+            key={item.label}
+          >
+            {item.label}
+          </Button>
+        );
+      })}
+    </div>
+  );
+}
